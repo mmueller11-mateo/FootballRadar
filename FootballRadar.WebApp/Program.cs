502a42213be64e167fa16d6337b9cd5d83c27e8e
Mateo Müller <mmueller11@online.gibz.ch>
2026-05-21 15:30:43 +0200

    Modify Program for Railway

--- FootballRadar.WebApp/Program.cs
+++ FootballRadar.WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 namespace FootballRadar.WebApp
 {
     public class Program
@@ -25,8 +27,11 @@ namespace FootballRadar.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
-            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseAuthentication();
